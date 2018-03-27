a09287c0afd7cad02be6ae435ead06cf503e0df3
Alex Pearson <3peara60@solent.ac.uk>
2018-03-27 12:45:45 +0100

    Unit Testing
    It has been started

---
+++ 3D_EscapeRoom/Assets/Editor/UnitTesting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+
+public class UnitTesting : MonoBehaviour
+{
+    [UnityTest]
+    public IEnumerator test1()
+    {
+        //Arrange
+
+        yield return null;
+
+        //Act
+
+        //Assert
+        Assert.AreEqual(1, 1);
+    }
+
+}
