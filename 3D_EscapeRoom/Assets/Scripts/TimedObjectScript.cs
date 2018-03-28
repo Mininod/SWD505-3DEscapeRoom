77904c54d385c1e2e114e210a685eecb8dfff6de
Alex Pearson <3peara60@solent.ac.uk>
2018-03-28 17:16:07 +0100

    SoundManager
    Implemented but not tested

---
+++ 3D_EscapeRoom/Assets/Scripts/TimedObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedObjectScript : MonoBehaviour
+{
+    public float maxLifetime;
+    private float lifetime = 0;
+    
+	void Update ()
+    {
+        lifetime += Time.deltaTime;
+
+        if (lifetime >= maxLifetime) Destroy(gameObject);
+	}
+}
