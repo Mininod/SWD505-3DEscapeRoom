5fc713a6feadbdf27e9818708de4d7238b3df9c6
Alex Pearson <3peara60@solent.ac.uk>
2018-04-03 13:05:55 +0100

    Collectable Objects
    Added a lot of collectable objects

--- 3D_EscapeRoom/Assets/Scripts/ObjectTypeScript.cs
+++ 3D_EscapeRoom/Assets/Scripts/ObjectTypeScript.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public enum objectType
 {
+    //For the system
     None,
+
+    //Stuffs
     CraftedExplosive,
     ExplosivePlantSpot,
     C4,
@@ -15,6 +18,26 @@ public enum objectType
     TestPickup,
     Key,
     FinalDoor,
+
+    //Official final items that can be picked up and used
+    Potato,
+    Battery,
+    SuperChargedPotato,
+    CircuitAdapter,
+    PotatoCircuit,
+    GunPowder,
+    ConductivePaste,
+    LockExplosive,
+    LockExplosivePlantSpot,         //to denote where to place the explosive (not actually collectable)
+    Shoe,
+    TitaniumSpray,
+    DoorStop,
+    DoorHandle,
+    Valve,
+    Spaghetti,
+    PickLock,
+    DuctTape,
+    MemoryStick
 }
 
 
